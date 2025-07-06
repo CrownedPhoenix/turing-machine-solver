67970bb0f802d1177a772d1485819a42f38a8bb4
Jairon Terrero <jairon@passivelogic.com>
2025-07-06 09:15:43 -0600

    refactor: Sort Solution display constraints

--- src/solver.rs
+++ src/solver.rs
@@ -13,7 +13,12 @@ pub struct Solution {
 
 impl std::fmt::Display for Solution {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let constraints = self.constraints.iter().map(ToString::to_string).join("; ");
+        let constraints = self
+            .constraints
+            .iter()
+            .sorted_by(|&a, &b| a.id.cmp(&b.id))
+            .map(ToString::to_string)
+            .join("; ");
         write!(f, "({}) with {constraints}", self.code)
     }
 }
